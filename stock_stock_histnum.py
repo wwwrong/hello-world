5154e1d39e6aa3c3272c8f3c0985b2cbdc3d52c8
wwwrong <wwwrong@163.com>
2017-01-22 15:01:03 +0800

    Create stock_stock_histnum.py

---
+++ stock_stock_histnum.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import string
+import os
+
+
+stocklist = open("stocklist.csv")
+stocksum = open("stocksum.csv",'w')
+lines = stocklist.readlines()
+for line in lines:
+    arr = line.replace("'",'').replace('\n','').split(',')
+    stockno = arr[0]
+    stocknmae = arr[1]
+    if stockno[0:2] in ('00','30'):
+        stockno = 'SZ'+stockno
+    else:
+        stockno = 'SH'+stockno        
+
+
+    filename = "./hist/"+stockno+".csv"
+    if not os.path.isfile(filename):
+        continue
+    stockhist = open(filename)
+    lines = stockhist.readlines()
+    stocksum.write(stockno+','+stocknmae+','+str(len(lines))+'\n')
+stocksum.close()
